f47e1c65cd78ad0e85b249629eb0527377e9f63c
Everton Ricardo <evertonricardo036@gmail.com>
2026-03-30 18:46:21 -0300

    Create calculateArea.js

---
+++ calculateArea.js
@@ -0,0 +1,7 @@
+function calculateArea(length, width) {
+  return length * width;
+}
+
+// Testes de saída no console
+console.log("Área (8x9):", calculateArea(8, 9)); 
+console.log("Área (3x5):", calculateArea(3, 5));
